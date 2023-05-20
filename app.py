5b574e2e31439f7da432583e73614369b5c69b70
victorinayaipi <124043799+victorinayaipi@users.noreply.github.com>
2023-05-20 17:03:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit
 import pandas
-import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 
 streamlit.title('My parents new healthy diner')
 
@@ -22,3 +19,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
